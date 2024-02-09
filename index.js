15c9600dbfd0c873ab3a9b74d8e4baf600bc5d7b
DUUDEIS <134599764+DUUDEIS@users.noreply.github.com>
2024-02-09 07:00:09 +0530

    Update index.

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Watching NEX HOSTING"];
 
 
 let currentIndex = 0;
